4415e130d3f4168631a6b4a921843916cbb8770a
Shaun Grady <grady@shaungrady.com>
2016-09-27 09:14:47 -0700

    Pull everything into config block, dropping run block
    Fixes #43

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ import httpDecorator from './httpDecorator'
 import httpInterceptor from './httpInterceptor'
 import cacheServiceAdapters from './cacheServiceAdapters'
 
-var _$provide
-
 export default angular
   .module('http-etag', [])
   .provider('httpEtag', service)
   .config(cacheServiceAdapters)
   .config(['$provide', '$httpProvider', function addHttpEtagInterceptor ($provide, $httpProvider) {
-    _$provide = $provide
-    $httpProvider.interceptors.push(httpInterceptor)
-
     httpDecorator.useLegacyPromiseExtensions =
       $httpProvider.useLegacyPromiseExtensions ||
       function useLegacyPromiseExtensions () { return true }
+    $provide.decorator('$http', httpDecorator)
+    $httpProvider.interceptors.push(httpInterceptor)
   }])
-  .run(function decorateHttpService () {
-    _$provide.decorator('$http', httpDecorator)
-  })
   .name
