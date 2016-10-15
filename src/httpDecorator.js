6eeafff7203c8626b806c58527603c25959f9355
Shaun Grady <grady@shaungrady.com>
2016-10-14 20:05:48 -0700

    Optimize check

--- src/httpDecorator.js
+++ src/httpDecorator.js
@@ -51,7 +51,7 @@ function httpEtagHttpDecorator ($delegate, httpEtag) {
 
       if (useLegacyPromiseExtensions) {
         httpPromise.cached = function httpEtagPromiseCached (callback) {
-          if (isCachable && rawCacheData && cacheInfo.cacheResponseData) {
+          if (rawCacheData && cacheInfo.cacheResponseData) {
             callback(cachedResponse, 'cached', undefined, httpConfig, itemCache)
           }
           return httpPromise
@@ -63,7 +63,7 @@ function httpEtagHttpDecorator ($delegate, httpEtag) {
       }
 
       httpPromise.ifCached = function httpEtagPromiseIfCached (callback) {
-        if (isCachable && rawCacheData && cacheInfo.cacheResponseData) {
+        if (rawCacheData && cacheInfo.cacheResponseData) {
           callback({
             data: cachedResponse,
             status: 'cached',
