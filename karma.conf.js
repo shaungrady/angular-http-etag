bc2713dcad4278a62f11892368b632484e11b0fe
Shaun Grady <grady@shaungrady.com>
2017-06-23 06:58:36 -0700

    Update `travis.yml` to run a build
    Some other tweaks, too

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,6 @@ module.exports = function (config) {
 
     port: 9876,
     colors: true,
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
